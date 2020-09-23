487d8ea81ff68897439b36a653dd5224ecc1b4a9
MJRAMZ <34893606+MJRAMZ@users.noreply.github.com>
2020-09-23 10:39:37 -0700

    added random module for pseudo-random functionality

--- hangman.py
+++ hangman.py
@@ -1,17 +1,27 @@
+# Importing random module for pseudo-random functionality
+import random
+
 display_message = """
 H A N G M A N
 """
-
 print(display_message)
 
-winning_word = "python"
+# List of random words to choose from
+word_list = ["python", "java", "kotlin", "javascript"]
+
+# Seeding the random behavior
+# No argument passed, seeding from current system time
+random.seed()
+
+# Shuffle sequence around then choose random word from list
+random.shuffle(word_list)
+winning_word = random.choice(word_list)
 
 # Prompt user for input
-print("Guess the word: ")
-guess = input()
+guess = input("Guess the word: ")
 
 # Check if  user guessed correctly
 if guess == winning_word:
     print("You survived!")
 else:
-    print("You are hanged!)
+    print("You are hanged!")
